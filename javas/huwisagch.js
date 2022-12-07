de2f48d0926301b2356594d18c2129ecf76889c1
BMunkhtur <116338079+BMunkhtur@users.noreply.github.com>
2022-12-07 13:48:59 +0800

    up

---
+++ javas/huwisagch.js
@@ -0,0 +1,38 @@
+let a = 85;
+let b = 75;
+let c = 96;
+let d = 69;
+sum=0;
+
+    if (a>80){
+        sum=sum+a;
+    }
+    if (b>80){
+        sum = sum+b;
+    }
+    if (c>80){
+        sum=sum+c;
+    }
+    if (d>80){
+        sum = sum+d;
+    }
+console.log(sum); //1
+
+let a1 = 3;
+let b1 = 7;
+let c1 = 2;
+let d1 = 4;
+sum1=1;
+    if (a1<5){
+        sum1 = sum1*a1;
+    }
+    if (b1<5){
+        sum1 = sum1*b1;
+    }
+    if (c1<5){
+        sum1 = sum1*c1;
+    }
+    if (d1<5){
+        sum1 = sum1*d1;
+    }
+    console.log(sum1) //2
