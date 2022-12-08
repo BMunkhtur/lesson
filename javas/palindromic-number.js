658e8f11e72ca067df5619394b5ead6ff99a9c75
BMunkhtur <116338079+BMunkhtur@users.noreply.github.com>
2022-12-08 12:20:52 +0800

    up

---
+++ javas/palindromic-number.js
@@ -0,0 +1,7 @@
+let number = prompt("duriin tooo")
+number = number + "";
+number1 = number.split("").reverse().join('');
+if (number == number1)
+    console.log("true");
+else
+    console.log("false");
