91f2c9ff2fd2261f2655f929958f14cc02fe84db
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-02-07 07:35:41 +0800

    up

--- jsLoop/dawharDawtalt/forfor.js
+++ jsLoop/dawharDawtalt/forfor.js
@@ -1,22 +1,22 @@
-let i , j, rows = parseInt(prompt("Duriin too oruulna uu?"));
-let  output = ``; 
-for( i=1; i<=rows; i++){       
-   output = ``;
-   for( j=1; j<i+1; j++){        
-       output +=`${j}`;
-   }
-    console.log(output)
-
+let i,
+  j,
+  rows = parseInt(prompt("Duriin too oruulna uu?"));
+let output = ``;
+for (i = 1; i <= rows; i++) {
+  output = ``;
+  for (j = 1; j < i + 1; j++) {
+    output += `${j}`;
+  }
+  console.log(output);
 }
-let i1, j1, rows1 = parseInt(prompt("duriin too")); 
+let i1,
+  j1,
+  rows1 = parseInt(prompt("duriin too"));
 let output1 = "";
-for ( i1 = 1; i1 <= rows1; i1++) {
-  for ( j1 = 1; j1 <= rows1 - i1 + 1; j1++) {
+for (i1 = 1; i1 <= rows1; i1++) {
+  for (j1 = 1; j1 <= rows1 - i1 + 1; j1++) {
     output1 += rows - j1 + 1;
   }
   output1 += "\n";
 }
 console.log(output1);
-
-
-
