adcc09d490267ba572583ca263100ae598196862
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-02-08 06:36:53 +0800

    up

---
+++ jsNumber/simpleAssignment/index.js
@@ -0,0 +1,8 @@
+const min = 0;
+const max = 100;
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(`  ${a}`);
+const min2 = 50;
+const max2 = 255;
+const b = Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
+console.log(`  ${b}`);
