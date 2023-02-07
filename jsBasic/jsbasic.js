4cb975353fbccd32039c32fe36a84c60e3b74caf
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-02-07 08:00:01 +0800

    up

---
+++ jsBasic/jsbasic.js
@@ -0,0 +1,94 @@
+//Comments can make code readable
+//Javascript lesson begin
+/*Comments can make code readable"*/
+
+let name = "Munkhtur";
+let surguuli = false;
+let oyutan = true;
+console.log(typeof 42);
+console.log(typeof "text");
+console.log(typeof true);
+console.log(typeof undeclaredVariable);
+console.log(typeof null);
+let firstname = "Bayarsaikhan";
+let lastname = "Munhtur";
+let maritalstatus = "single";
+let country = "Ulaanbaatar";
+let age = 21;
+let firstname1 = "bayarsaikahn";
+name1 = "Munkhtur";
+maritalstatus = "Single";
+country = "Ulaanbaatar";
+age = 21;
+function findPerimeter(a, b, c) {
+  return a + b + c;
+}
+let a = 2,
+  b = 3,
+  c = 5;
+let pri = 10;
+console.log(findPerimeter(a, b, c));
+function findArea(r) {
+  return pri * r * r;
+}
+let r, Area;
+r = 5;
+Area = findArea(r);
+console.log("Area of Circle is: " + Area);
+let x = 5;
+let y = 2;
+let m = y * 2 - ((y * 1) / x) * 2 - x * 1;
+console.log(y * 2 - ((y * 1) / x) * 2 - x * 1);
+let gb = 15;
+function findGigabyte(mb, kb, bytes, bit) {
+  return mb * kb * bytes * bit * gb;
+}
+let mb = 1024,
+  kb = 1024,
+  bytes = 1024,
+  bit = 8;
+console.log(findGigabyte(mb, kb, bytes, bit));
+let celsius0 = 32;
+function findfahre(ce, celsius0) {
+  return (ce * 9) / 5 + celsius0;
+}
+let ce = 4;
+console.log(findfahre(ce, celsius0));
+
+let fahre0 = -17.77778;
+function findcelsius(ce, celsius0) {
+  return ((fahre - 32) * 5) / 9;
+}
+let fahre = 1;
+console.log(findcelsius(ce, celsius0));
+let cm1 = 0.3937007874;
+function findinch(cm, inch) {
+  return cm1 * inch;
+}
+let inch = 4;
+console.log(findinch(cm1, inch));
+let inch1 = 2.54;
+function findinch(inch1, cm) {
+  return inch1 * cm;
+}
+let cm = 4;
+console.log(findinch(inch1, cm));
+let baby1 =
+  "baby shark, doo-doo, doo-doo baby shark, doo-doo, doo-doo baby shark, doo-doo, doo-doo baby shark";
+let baby2 =
+  "Mommy Shark, doo-doo, doo-doo Mommy Shark, doo-doo, doo-doo Mommy Shark, doo-doo, doo-doo Mommy shark";
+let baby3 =
+  "Daddy Shark, doo-doo, doo-doo Daddy Shark, doo-doo, doo-doo Daddy Shark, doo-doo, doo-doo Daddy Shark";
+let baby4 =
+  "Grandma Shark, doo-doo, doo-doo Grandma Shark, doo-doo, doo-doo Grandma Shark, doo-doo, doo-doo Grandma Shark";
+let baby5 =
+  "Grandpa Shark, doo-doo, doo-doo Grandpa Shark, doo-doo, doo-doo Grandpa Shark, doo-doo, doo-doo Grandpa Shark";
+let baby6 =
+  "Let's go hunt, doo-doo, doo-doo Let's go hunt, doo-doo, doo-doo Let's go hunt, doo-doo, doo-doo Let's go hunt";
+let baby7 =
+  "Run away, doo-doo, doo-doo Run away, doo-doo, doo-doo Run away, doo-doo, doo-doo Run away";
+let baby8 =
+  "Safe at last, doo-doo, doo-doo Safe at last, doo-doo, doo-doo Safe at last, doo-doo, doo-doo Safe at last";
+let baby9 =
+  "It's the end, doo-doo, doo-doo It's the end, doo-doo, doo-doo It's the end, doo-doo, doo-doo It's the end";
+console.log(baby1, baby2, baby3, baby4, baby5, baby6, baby7, baby8, baby9);
